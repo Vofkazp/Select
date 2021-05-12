a38f66862d10a27eb8fde3007ba259859faa4489
Vofka <vofkazp@gmail.com>
2021-05-12 10:28:58 +0300

    final

--- js/select.js
+++ js/select.js
@@ -4,6 +4,10 @@ document.querySelectorAll(".select").forEach((e) => {
     input = e.querySelector(".select-input"),
     select = e.querySelector(".select-block"),
     back = e.querySelector(".select-back");
+  let index = 0;
+
+  items[index].classList.add("active");
+  select.innerHTML = items[index].textContent;
 
   select.addEventListener("click", () => {
     list.classList.toggle("block");
@@ -11,9 +15,34 @@ document.querySelectorAll(".select").forEach((e) => {
   });
 
   select.addEventListener("keydown", function (event) {
-    if (event.code == "Enter" || event.code == "Space") {
-        list.classList.toggle("block");
-        back.classList.toggle("block");
+    if (list.classList.contains("block")) {
+      switch (event.code) {
+        case "ArrowUp":
+          move("up");
+          select.innerHTML = items[index].textContent;
+          input.value = items[index].value;
+          break;
+        case "ArrowDown":
+          move("down");
+          select.innerHTML = items[index].textContent;
+          input.value = items[index].value;
+          break;
+        case "Enter":
+          list.classList.toggle("block");
+          back.classList.toggle("block");
+          break;
+        case "Escape":
+          list.classList.toggle("block");
+          back.classList.toggle("block");
+          break;
+        case "Tab":
+          list.classList.toggle("block");
+          back.classList.toggle("block");
+          break;
+      }
+    } else if (event.code == "Enter" || event.code == "Space") {
+      list.classList.toggle("block");
+      back.classList.toggle("block");
     }
   });
 
@@ -22,12 +51,35 @@ document.querySelectorAll(".select").forEach((e) => {
     back.classList.toggle("block");
   });
 
-  items.forEach((el) => {
+  items.forEach((el, i) => {
     el.addEventListener("click", () => {
       select.innerHTML = el.textContent;
       input.value = el.value;
       list.classList.toggle("block");
       back.classList.toggle("block");
+      items.forEach((d) => {
+        d.classList.remove("active");
+      });
+      el.classList.add('active');
+      index = i;
     });
   });
+
+  function move(dir) {
+    items.forEach((d) => {
+      d.classList.remove("active");
+    });
+    !items[index] || !items[index].classList.contains("active");
+    switch (dir) {
+      case "up":
+        index = index <= 0 ? items.length - 1 : --index;
+        break;
+      case "down":
+        index = index == items.length - 1 ? 0 : ++index;
+        break;
+      default:
+        throw Error("WTF?");
+    }
+    items[index].classList.add("active");
+  }
 });
